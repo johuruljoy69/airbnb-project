7b4390d1104e43f1953a52ee253ab832b1b56c2a
johirjoy <joy.sd69@gmail.com>
2023-06-17 13:57:04 +0600

    login page create and login form setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './contexts/AuthProvider'
-import { router } from './routes/routes'
 import { Toaster } from 'react-hot-toast'
+import { router } from './routes/routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
